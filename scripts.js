e5da9334431efa1f223b7c1fb2a87bf7a35e0a4f
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-05-07 15:24:30 +0200

    Input is empty

--- scripts.js
+++ scripts.js
@@ -5,5 +5,13 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+
+  // Check if either input is empty
+  if (dividend === "" || divider === "") {
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again.";
+    return; // Exit the function to prevent further execution
+  }
+
   result.innerText = Math.floor(dividend / divider);
 });
